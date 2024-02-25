ccdf7fd4d3b3e35356ee5a91f0466cf705ac8b75
sarika pokhrel <https://github.com/Sarika18-v>
2024-02-25 10:01:29 +0545

    Day4

---
+++ week1/python/day4/assignmentsonstring/length.py
@@ -0,0 +1,3 @@
+#Use the len method to print the length of string.
+name="Sarika"
+print(len(name))
---
+++ week1/python/day4/assignmentsonstring/length1.py
@@ -0,0 +1,6 @@
+#Print length of String without using len function.
+name= "Sarika"
+counter = 0
+for s in name:
+        counter = counter+1
+print("Length of string is:", counter)
