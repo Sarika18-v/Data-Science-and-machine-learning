21a02c781646806005e73d0bab763d05d9448f4f
sarika pokhrel <https://github.com/Sarika18-v>
2024-03-21 21:59:06 +0545

    week2

---
+++ week2/week2day5and6/class/polymorphism/prog1.py
@@ -0,0 +1,11 @@
+class French:
+    def sayhello(self):
+        print("hello french")
+class Nepali:
+    def sayhello(self):
+        print("namaskar")
+def greet(lang):
+    lang.sayhello()
+if __name__:"__main__"
+French=French()
+greet(French)
